c81d7ff6e0d74553a047b6793e5b0d685fe70276
ala ben hassine <ala.benhassine@esprit.tn>
2021-12-10 16:11:31 +0100

    modification 0 de ala

---
+++ src/main/java/com/vermeg/g4/EtudiantAla.java
@@ -0,0 +1,54 @@
+package com.vermeg.g4;
+
+public class EtudiantAla {
+
+	private int id;
+	private String nompre;
+	private int age;
+	
+	
+	
+	
+	public EtudiantAla() {
+		
+	}
+
+	public EtudiantAla(int id, String nompre, int age) {
+		super();
+		this.id = id;
+		this.nompre = nompre;
+		this.age = age;
+	}
+	
+	public int getId() {
+		return id;
+		
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNompre() {
+		return nompre;
+	}
+	public void setNompre(String nompre) {
+		this.nompre = nompre;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "EtudiantAla [id=" + id + ", nompre=" + nompre + ", age=" + age + "]";
+	}
+	
+	
+	
+	
+	
+
+}
