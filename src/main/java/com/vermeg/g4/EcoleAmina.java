8ee262f864413920e16a9af6276cd89f49c4f7e4
Amina CHABCHOUB <chabchoub.amina@esprit.tn>
2021-12-10 16:11:17 +0100

    modification0Amina

---
+++ src/main/java/com/vermeg/g4/EcoleAmina.java
@@ -0,0 +1,60 @@
+package com.vermeg.g4;
+
+import java.util.Objects;
+
+public class EcoleAmina {
+	private int id ; 
+	private String nom ; 
+	private String adrres ; 
+	private int num_tel ;
+	public EcoleAmina(int id, String nom, String adrres, int num_tel) {
+		this.id = id;
+		this.nom = nom;
+		this.adrres = adrres;
+		this.num_tel = num_tel;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdrres() {
+		return adrres;
+	}
+	public void setAdrres(String adrres) {
+		this.adrres = adrres;
+	}
+	public int getNum_tel() {
+		return num_tel;
+	}
+	public void setNum_tel(int num_tel) {
+		this.num_tel = num_tel;
+	}
+	@Override
+	public String toString() {
+		return "EcoleAmina [id=" + id + ", nom=" + nom + ", adrres=" + adrres + ", num_tel=" + num_tel + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, num_tel);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EcoleAmina other = (EcoleAmina) obj;
+		return id == other.id && num_tel == other.num_tel;
+	} 
+
+}
