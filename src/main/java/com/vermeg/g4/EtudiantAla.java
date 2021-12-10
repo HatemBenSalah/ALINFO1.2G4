7c95f8fed943d0cb3786e21e844b3f8f3851ea53
ala1235 <ala.benhassine@esprit.tn>
2021-12-10 17:55:23 +0100

    moification ToString Attribut Const

--- src/main/java/com/vermeg/g4/EtudiantAla.java
+++ src/main/java/com/vermeg/g4/EtudiantAla.java
@@ -5,11 +5,13 @@ public class EtudiantAla {
 	private int id;
 	private String nompre;
 	private int age;
+	private int numTel; 
 	
 	
 	//ala1
 	//ala2
 	//modif 4 alkhls
+	//ajouter attribut et modifier to string et constructeurs. 
 	
 	public EtudiantAla() {
 		
@@ -22,6 +24,16 @@ public class EtudiantAla {
 		this.age = age;
 	}
 	
+	
+	
+	public EtudiantAla(int id, String nompre, int age, int numTel) {
+		super();
+		this.id = id;
+		this.nompre = nompre;
+		this.age = age;
+		this.numTel = numTel;
+	}
+
 	public int getId() {
 		return id;
 		
@@ -42,11 +54,23 @@ public class EtudiantAla {
 	public void setAge(int age) {
 		this.age = age;
 	}
+	
+	
+
+	public int getNumTel() {
+		return numTel;
+	}
+
+	public void setNumTel(int numTel) {
+		this.numTel = numTel;
+	}
 
 	@Override
 	public String toString() {
-		return "EtudiantAla [id=" + id + ", nompre=" + nompre + ", age=" + age + "]";
+		return "EtudiantAla [id=" + id + ", nompre=" + nompre + ", age=" + age + ", numTel=" + numTel + "]";
 	}
+
+	
 	
 	
 	
