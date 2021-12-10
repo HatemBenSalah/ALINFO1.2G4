6f52cc991a289e64d9ac7635392ca18f40a01df5
hatem bensalah <hatembensalh@gmail.com>
2021-12-10 17:27:55 +0100

    commit 4

---
+++ src/main/java/com/vermeg/g4/Local.java
@@ -0,0 +1,68 @@
+package com.vermeg.g4;
+
+public class Local {
+	String adresse;
+	String name;
+	String gouvernement;
+	public Local() {
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((gouvernement == null) ? 0 : gouvernement.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Local other = (Local) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (gouvernement == null) {
+			if (other.gouvernement != null)
+				return false;
+		} else if (!gouvernement.equals(other.gouvernement))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGouvernement() {
+		return gouvernement;
+	}
+	public void setGouvernement(String gouvernement) {
+		this.gouvernement = gouvernement;
+	}
+	
+
+}
