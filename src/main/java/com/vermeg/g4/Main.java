7216ed2b7447814ec4de439e229cd42c06b309a9
hatem bensalah <hatembensalh@gmail.com>
2021-12-11 20:21:35 +0100

    branche hatembensalah and master merged  with commande --squash

--- src/main/java/com/vermeg/g4/Main.java
+++ src/main/java/com/vermeg/g4/Main.java
@@ -4,6 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+      Local espritchargia =new Local("chargia","esprit cours de soire","Ariena");
+      System.out.print(espritchargia);
+      Local espritghazela =new Local("Gazela","esprit cours du jour","Ariena");
+      System.out.print(espritghazela);
+      if(espritchargia.equals(espritghazela))
+      {
+          System.out.print("meme local");
+
+      }
+      else
+      {
+          System.out.print("n'est pas le meme local");
+
+      }
 
 	}
 
