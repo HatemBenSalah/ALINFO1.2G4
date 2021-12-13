f27b984cfea9a0b04ad6f9fd244c8399b2a84687
ala1235 <ala.benhassine@esprit.tn>
2021-12-13 14:33:03 +0100

    merge2

--- src/main/java/com/vermeg/g4/EtudiantAla.java
+++ src/main/java/com/vermeg/g4/EtudiantAla.java
@@ -12,7 +12,7 @@ public class EtudiantAla {
 	//ala2
 	//modif 4 alkhls
 	//ajouter attribut et modifier to string et constructeurs. 
-	//5eme Modif dimanche
+	//5eme 1Modif dimanche
 	
 	public EtudiantAla() {
 		
