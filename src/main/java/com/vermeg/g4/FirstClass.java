49e6902c2e077e4947f41f0844877ec8fc0ffbd7
Med malek chamakh <medmalek.chamakh@esprit.tn>
2021-12-13 23:21:32 +0100

    test egaliter

--- src/main/java/com/vermeg/g4/FirstClass.java
+++ src/main/java/com/vermeg/g4/FirstClass.java
@@ -5,6 +5,13 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Responsable resp =new Responsable(98,"Chamakh","Med_malek","25/12/1998");
+		System.out.println(resp);
+		Responsable resp1 =new Responsable(101,"bensalah","Hatem","14/10/1998");
+		System.out.println(resp1);
+		if (resp.equals(resp1))
+		{
+			System.out.println("responsable egaux");
+		}
 	}
 
 }
