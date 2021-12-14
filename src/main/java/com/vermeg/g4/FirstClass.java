288945a4b24c63ffbd8dd99c40365f0104d48591
abbesimen <imen.abbes012@gmail.com>
2021-12-14 23:08:09 +0100

    commit

--- src/main/java/com/vermeg/g4/FirstClass.java
+++ src/main/java/com/vermeg/g4/FirstClass.java
@@ -3,14 +3,14 @@ package com.vermeg.g4;
 public class FirstClass {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		// TODO Auto-generated method stub
        System.out.print("test");
        
-=======
+
          Local l =new Local();
          System.out.print(l);
->>>>>>> origin/hatembensalah
+
 	}
 // test
 }
